1523f3a2bb47cfce9f53904d0a7a29e891d5de93
KeslieBox <kesliebox@gmail.com>
2021-03-01 12:20:00 -0600

    Done.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,17 +6,27 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do 
+    # We use ERB to get all of the pets with `Pet.all`, then we iterate over that
+    #collection of `Pet` objects and generate a checkbox for each pet.
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do 
-    
+    @owner = Owner.create(params[:owner])
+
+    if !params[:pet][:name].empty?
+      @owner.pets << Pet.create(name: params[:pet][:name])
+    end
+    redirect "/owners/#{@owner.id}"
   end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
+    @pets = Pet.all
     erb :'/owners/edit'
   end
+
 
   get '/owners/:id' do 
     @owner = Owner.find(params[:id])
@@ -24,6 +34,17 @@ class OwnersController < ApplicationController
   end
 
   patch '/owners/:id' do 
-   
+    ####### bug fix
+    if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+      end
+      #######
+  
+      @owner = Owner.find(params[:id])
+      @owner.update(params["owner"])
+      if !params["pet"]["name"].empty?
+        @owner.pets << Pet.create(name: params["pet"]["name"])
+      end
+      redirect "owners/#{@owner.id}"
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,51 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
+
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @owners = Owner.all
+    @pet = Pet.create(params[:pet])
+   
+    if !params[:owner][:name].empty?
+      owner = Owner.create(params[:owner]) 
+      @pet.owner = owner
+      @pet.save
+    end
 
     redirect to "pets/#{@pet.id}"
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find_by(id: params[:id])
+    @owners = Owner.all
+    @owner = Owner.find_by(id: params[:id])
+
+    erb :'/pets/edit'
+  end
+
   get '/pets/:id' do 
-    @pet = Pet.find(params[:id])
+    @pet = Pet.find_by(id: params[:id])
+    @owner = Owner.find_by(id: params[:id])
     erb :'/pets/show'
   end
 
   patch '/pets/:id' do 
-
+     ####### bug fix
+     if !params[:pet].keys.include?("owner_id")
+      params[:pet]["owner_id"] = []
+      end
+      #######
+  
+      @pet = Pet.find(params[:id])
+      @pet.update(params[:pet])
+      if !params[:owner][:name].empty?
+        binding.pry
+        @owner.pets << Pet.create(name: params["pet"]["name"])
+      end
     redirect to "pets/#{@pet.id}"
   end
 end
